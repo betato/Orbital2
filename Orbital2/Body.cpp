98d541f158c77165e4988c674018d636c89d1f4e
betato <battlestargatticus@gmail.com>
2018-06-29 12:18:05 -0400

    Fix body movement

--- Orbital2/Body.cpp
+++ Orbital2/Body.cpp
@@ -8,11 +8,11 @@ void Body::addForce(sf::Vector2f appliedForce)
 void Body::move(float dt)
 {
 	// a = f/M
-	acceleration.x = mass/force.y;
-	acceleration.y = mass/force.x;
+	acceleration.x = force.y / mass;
+	acceleration.y = force.x / mass;
 
-	velocity = acceleration * dt;
-	position = velocity * dt;
+	velocity += acceleration * dt;
+	position += velocity * dt;
 }
 
 Body::Body(const Shape shape, float mass, float restitution) : shape(shape)
--- Orbital2/Game.cpp
+++ Orbital2/Game.cpp
@@ -4,11 +4,12 @@
 
 #include "Display.h"
 
-#define TIMESTEP 1
+#define TIMESTEP 0.025
 
 Game::Game()
 {
 	bodies.push_back(new Circle(1, 0.5, 10));
+	bodies[0]->addForce(sf::Vector2f(-2, 2));
 }
 
 Game::~Game()
@@ -23,21 +24,25 @@ void Game::input(const sf::Event & events)
 
 void Game::update()
 {
+	// Check Collisions
+	for (std::size_t i = 0; i < bodies.size(); i++)
+	{
+		for (size_t j = i; j < bodies.size(); j++)
+		{
+			if (bodies[i]->shape == Shape::CIRCLE && bodies[j]->shape == Shape::CIRCLE)
+			{
+
+			}
+			else if (bodies[i]->shape == Shape::RECTANGLE && bodies[j]->shape == Shape::RECTANGLE)
+			{
+
+			}
+		}
+	}
+
+	// Move
 	for (Body* body : bodies)
 	{
-		// Collide
-		switch (body->shape)
-		{
-		case Shape::CIRCLE :
-			
-			break;
-		case Shape::RECTANGLE:
-
-			break;
-		}
-		Display::draw(*body);
-
-		// Move
 		body->move(TIMESTEP);
 	}
 }
