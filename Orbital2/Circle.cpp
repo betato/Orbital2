03e0bc0cde6067758bb8392ac0b3bb1fc352f277
betato <battlestargatticus@gmail.com>
2018-06-29 17:31:09 -0400

    Add velocity vector display to circle and rectangle

--- Orbital2/Circle.cpp
+++ Orbital2/Circle.cpp
@@ -13,4 +13,12 @@ void Circle::draw(sf::RenderTarget & target, sf::RenderStates states) const
 	states.transform.translate(sf::Vector2f(position.x - radius, position.y - radius));
 	// Draw vertex array
 	target.draw(drawCircle, states);
+
+	// Temporary velocity vector
+	sf::VertexArray v;
+	states.transform.translate(sf::Vector2f(radius, radius));
+	v.setPrimitiveType(sf::PrimitiveType::Lines);
+	v.append(sf::Vertex(sf::Vector2f(0, 0), sf::Color::Red));
+	v.append(sf::Vertex(velocity, sf::Color::Red));
+	target.draw(v, states);
 }
--- Orbital2/Rectangle.cpp
+++ Orbital2/Rectangle.cpp
@@ -10,8 +10,16 @@ Rectangle::Rectangle() : Rectangle(0, 0, sf::Vector2f(0, 0)) { }
 void Rectangle::draw(sf::RenderTarget & target, sf::RenderStates states) const
 {
 	sf::RectangleShape drawRect(size);
-	states.transform.translate(sf::Vector2f(position.x - size.x * 0.5, position.y - size.y * 0.5));
+	states.transform.translate(sf::Vector2f(position.x - size.x * 0.5f, position.y - size.y * 0.5f));
 	// Draw vertex array
 	target.draw(drawRect, states);
+
+	// Temporary velocity vector
+	sf::VertexArray v;
+	states.transform.translate(sf::Vector2f(size.x * 0.5f, size.y * 0.5f));
+	v.setPrimitiveType(sf::PrimitiveType::Lines);
+	v.append(sf::Vertex(sf::Vector2f(0, 0), sf::Color::Red));
+	v.append(sf::Vertex(velocity, sf::Color::Red));
+	target.draw(v, states);
 }
 
